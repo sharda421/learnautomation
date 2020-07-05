c0f857a1adc0201bbd2ee5d84a3287ce0c1c93b2
NXS8390 <NXS8390@nxs8390PF1PNZN4.us.tgr.net>
2020-07-05 12:45:36 -0500

    This is my first commit

---
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,39 @@
+package example;
+		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.AfterTest;
+
+
+public class NewTest {
+	
+	WebDriver driver;
+  
+	@Parameters({"browser","url"})
+	@Test
+	public void firstTest(String browser, String url) {
+		System.out.println("Parameter passed is Chrome");
+	    driver.get(url);  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Guru99")); 		
+  }
+  
+	//"browser" is the variable from pom.xml
+  
+  @BeforeTest
+  public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\nxs8390\\Documents\\Automation\\Selenium\\chromedriver.exe");
+		driver=new ChromeDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();	
+  }
+
+}
